550e3128cc4a1a02f4896f157fc757fab7d6dfc8
pandaachu <pandaachu@gmail.com>
2020-10-29 01:08:40 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,7 +4,7 @@ var mySwiper = new Swiper('.heroBanner__swiper', {
   speed: 1000,
   spaceBetween: 100,
   loop: true,
-   // Navigation arrows
+  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -15,41 +15,58 @@ var mySwiper = new Swiper('.choices__swiper', {
   speed: 1000,
   // spaceBetween: 100,
   loop: true,
-   // Navigation arrows
+  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  slidesPerView: 4,
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //   },
-  //   768: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40,
-  //   },
-  //   1024: {
-  //     slidesPerView: 5,
-  //     spaceBetween: 50,
-  //   },
-  // }
+  // slidesPerView: 4,
+  breakpoints: {
+    414: {
+      slidesPerView: 2.2,
+      spaceBetween: 20,
+    },
+    576: {
+      slidesPerView: 2.2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
 });
 
-
 // date range picker
-$(document).ready(function(){
+$(document).ready(function () {
+  var dates = new Date();
+  var beginTimeStore = dates.getFullYear()+'-'+(dates.getMonth()+1)+'-'+dates.getDate(); // 獲取當前日期
+  var endTimeStore = '';
   $('input[name="js-datePicker"]').daterangepicker({
-    autoApply: true,
+    minDate: beginTimeStore, // 設置最小選擇日期
+    autoApply: true, // 關掉確認欄位及選好日期立即關掉畫面
     // opens: "left",
     autoUpdateInput: false,
     locale: {
+      format: 'YYYY-MM-DD',
       cancelLabel: 'Clear',
       daysOfWeek:["S","M","T","W","T","F","S"], // 也可以改成中文
       monthNames: ["January","February","March","April","May","June","July","August","September","October","November","December"],
-    },
+    }
+  }, function(start, end, label) {
+    beginTimeStore = start;
+    endTimeStore = end;
+    if(!this.startDate){
+      this.element.val('');
+    }else{
+      this.element.val(this.startDate.format(this.locale.format) + this.locale.separator + this.endDate.format(this.locale.format));
+    }
   });
+  // input欄位原始值
   $('input[name="js-datePicker"]').on('apply.daterangepicker', function(ev, picker) {
     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
   });
@@ -58,4 +75,3 @@ $(document).ready(function(){
       $(this).val('');
   });
 });
-
